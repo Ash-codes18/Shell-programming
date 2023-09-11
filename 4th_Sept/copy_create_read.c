c6471945769c424e246dc3561ecccfd87c8a37a1
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-09-11 08:38:48 +0530

    update 5 files

--- 4th_Sept/copy_create_read.c
+++ 4th_Sept/copy_create_read.c
@@ -6,17 +6,24 @@
 int main(){
 
     int a,b,c;
-    char buf[100];
+    char buf[1024];
 
-    a=open("first_half.txt",O_CREAT|O_RDWR,0777);
+    a=open("first_half.txt",O_CREAT|O_WRONLY,0644);
     b=open("second_half.txt",O_CREAT|O_RDWR,0777);
-    c=open("file.txt",O_RDWR,0777);
+    c=open("file.txt",O_RDONLY);
 
     int size=lseek(c,0,SEEK_END);
     int fh=size/2;
 
-    int x=read(c,buf,fh);
-    write(a,buf,x);
+    read(c,buf,fh);
+    write(a,buf,fh);
+    lseek(a,fh,SEEK_SET);
+    read(c,buf,size-fh);
+    write(b,buf,size-fh);
+
+    close(a);
+    close(b);
+    close(c);
     
 
     return 0;
--- calculator.sh
+++ calculator.sh
@@ -2,7 +2,7 @@ echo "Enter a no. :"
 read a
 echo "Enter another no. :"
 read b
-echo "List of operations available : 1.)Addition  2.)Subtraction  3.)Multiplication"
+echo -e "\nList of operations available :\n1.)Addition\n2.)Subtraction\n3.)Multiplication\n4.)Division\n"
 echo "Enter your choice : "
 read c
 
@@ -19,4 +19,8 @@ fi
 if [ "$c" -eq 3 ]
 then 
     echo "Product : "$(( a * b ))
+fi
+if [ "$c" -eq 4 ]
+then
+    echo "Quotient : "`expr $a / $b`
 fi
